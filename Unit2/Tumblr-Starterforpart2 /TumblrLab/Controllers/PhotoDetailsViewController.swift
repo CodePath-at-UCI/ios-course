b0cb171a2ccde36c7bfb3811001b51fb5b938b7f
Chase Carnaroli <chasecarnaroli@gmail.com>
2020-01-27 20:40:09 -0800

    Created Unit 3

---
+++ Unit2/Tumblr-Starterforpart2 /TumblrLab/Controllers/PhotoDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoDetailsViewController.swift
+//  TumblrLab
+//
+//  Created by Chase Carnaroli on 1/23/20.
+//  Copyright © 2020 Lily Pham. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
